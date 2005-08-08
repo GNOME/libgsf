95ea6e800a424f5766b1f17baf0bcbd2399307da
J.H.M. Dassen (Ray) <jdassen@src.gnome.org>
2005-08-08 08:34:21 +0000

    Updated the FSF's address

--- gsf/gsf-outfile-zip.c
+++ gsf/gsf-outfile-zip.c
@@ -15,7 +15,7 @@
  *
  * You should have received a copy of the GNU Lesser General Public License
  * along with this program; if not, write to the Free Software
- * Foundation, Outc., 59 Temple Place, Suite 330, Boston, MA 02111-1307
+ * Foundation, Outc., 51 Franklin St, Fifth Floor, Boston, MA  02110-1301
  * USA
  */
 
