237c07a8b7831978f525834fcb3fc1b99457e8dc
Morten Welinder <terra@gnome.org>
2016-04-18 15:31:05 -0400

    tar: fix reading of mode etc.
    It's unclear how octal fields are to be terminated.  Allow both space
    and NUL.

--- gsf/gsf-infile-tar.c
+++ gsf/gsf-infile-tar.c
@@ -98,21 +98,30 @@ typedef struct {
 static gint64
 unpack_octal (GsfInfileTar *tar, const char *s, size_t len)
 {
-	gint64 res = 0;
+	guint64 res = 0;
+
+	/*
+	 * Different specifications differ on what terminating characters
+	 * are allowed.  It doesn't hurt for us to allow both space and
+	 * NUL.
+	 */
+	if (len == 0 || (s[len - 1] != 0 && s[len - 1] != ' '))
+		goto invalid;
+	len--;
 
 	while (len--) {
 		unsigned char c = *s++;
-		if (c == 0)
-			break;
-		if (c < '0' || c > '7') {
-			tar->err = g_error_new (gsf_input_error_id (), 0,
-						_("Invalid tar header"));
-			return 0;
-		}
-		res = (res << 3) + (c - '0');
+		if (c < '0' || c > '7')
+			goto invalid;
+		res = (res << 3) | (c - '0');
 	}
 
-	return res;
+	return (gint64)res;
+
+invalid:
+	tar->err = g_error_new (gsf_input_error_id (), 0,
+				_("Invalid tar header"));
+	return 0;
 }
 
 
